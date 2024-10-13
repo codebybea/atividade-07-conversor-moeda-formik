a914e1ab0a828881b5717e18688fe8e406d6ba50
codebybea <beatriz.borges0407@gmail.com>
2024-10-13 19:54:11 -0300

    add atv

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,116 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import Pagina from "@/components/Pagina";
+import { Formik } from "formik";
+import { useState } from "react";
+import { Button, Form, Modal, Container, Card, Row } from "react-bootstrap";
+import { FaCheck, FaTrashAlt } from "react-icons/fa";
+
+// Taxa
+const taxaConversao = {
+  dolar: 0.2,
+  euro: 0.18,
+  bitcoin: 0.000003,
+};
+
+export default function page() {
+  const [showModal, setShowModal] = useState(false);
+  const [resultado, setResultado] = useState(0);
+
+  // Mudar Imagem
+  const getImagePath = (moeda) => {
+    switch (moeda) {
+      case "dolar":
+        return "/dolar.png";
+      case "euro":
+        return "/euro.png";
+      case "bitcoin":
+        return "/bitcoin.png";
+      default:
+        return "/mood.png";
+    }
+  };
+
+  function converter(values) {
+    const valorConvertido = values.real * taxaConversao[values.moeda];
+    setResultado(
+      valorConvertido.toLocaleString("pt-BR", { minimumFractionDigits: 2 })
+    );
+    setShowModal(true);
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Pagina titulo="Converter Moedas">
+      {/* Formulario */}
+      <Formik
+        initialValues={{
+          real: 0.0,
+          moeda: "",
+        }}
+        onSubmit={converter}
+      >
+        {({ values, handleChange, handleSubmit, handleReset }) => (
+          <Form>
+            <Container className="py-2 text-center w-25">
+              <Card.Img src={getImagePath(values.moeda)} />
+            </Container>
+            {/* // Container Formulário */}
+            <Row md={2}>
+              <Container>
+                <Form.Group className="mb-2">
+                  <Form.Label>Real:</Form.Label>
+                  <Form.Control
+                    name="real"
+                    type="number"
+                    value={values.real}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+                <Form.Group className="mb-2">
+                  <Form.Label>Moeda:</Form.Label>
+                  <Form.Select
+                    name="moeda"
+                    value={values.moeda}
+                    onChange={handleChange}
+                  >
+                    <option disabled value="">
+                      Selecione
+                    </option>
+                    <option value="dolar">Dólar</option>
+                    <option value="euro">Euro</option>
+                    <option value="bitcoin">Bitcoin</option>
+                  </Form.Select>
+                </Form.Group>
+
+                <Form.Group className=" py-4 mb-2 text-center">
+                  <Button onClick={handleSubmit} className="me-2">
+                    <FaCheck /> Enviar
+                  </Button>
+                  <Button onClick={handleReset}>
+                    <FaTrashAlt /> Limpar
+                  </Button>
+                </Form.Group>
+              </Container>
+            </Row>
+          </Form>
+        )}
+      </Formik>
+
+      {/* Modal do resultado */}
+      <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Resultado</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Conversão: {resultado}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Fechar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Pagina>
   );
 }
